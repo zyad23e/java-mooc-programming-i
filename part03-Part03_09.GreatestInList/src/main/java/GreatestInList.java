aacc8bc847a46a96bcaad4ceb3dfcb42cc23e279
zyad23e <zyade23@arizona.edu>
2025-08-08 15:38:01 -0700

    Progress: Completed more Java MOOC exercises

---
+++ part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class GreatestInList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+        
+        System.out.println("");
+
+        // implement finding the greatest number in the list here
+        System.out.println("The greatest number: " + Collections.max(list));
+    }
+}
