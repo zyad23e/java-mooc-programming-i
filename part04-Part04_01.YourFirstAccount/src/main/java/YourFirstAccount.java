decac064b7cbadfd493daf89e4a1d19787e1028b
zyad23e <zyade23@arizona.edu>
2025-08-12 09:13:49 -0700

    Progress: Completed more Java MOOC part 4 exercises

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,12 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account mine = new Account("Me", 100.0);
+        mine.deposit(20.0);
+        System.out.println(mine);
+    }
+}
---
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -0,0 +1,15 @@
+
+public class YourFirstBankTransfer {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // write your program here
+        
+        Account matt = new Account("Matthews account", 1000.0);
+        Account mine = new Account("My account", 0.0);
+        matt.withdrawal(100.0);
+        mine.deposit(100.0);
+        System.out.println(matt);
+        System.out.println(mine);
+    }
+}
