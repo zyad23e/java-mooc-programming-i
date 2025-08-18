c12ff0dd6344089f4a50a272c9da639ac5691bdc
zyad23e <zyade23@arizona.edu>
2025-08-18 09:25:23 -0700

    Progress: Completed more Java MOOC part 6 exercises

---
+++ part06-Part06_03.MessagingService/src/main/java/MessagingService.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zyade
+ */
+
+import java.util.ArrayList;
+
+public class MessagingService {
+    
+    private ArrayList<Message> messages = new ArrayList<>();
+    
+    public void add(Message message){
+        if (message.getContent().length() < 281){
+            messages.add(message);
+        }
+    }
+    
+    public ArrayList<Message> getMessages(){
+        return this.messages;
+    }
+    
+}
