a25cb0659934cf37a59c787c0a3d5609ace76e4a
zyad23e <zyade23@arizona.edu>
2025-08-18 10:13:35 -0700

    Progress: Completed more Java MOOC part 6 exercises

---
+++ part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+    
+    public String longest(){
+        if (elements.isEmpty()){
+            return null;
+        }
+        String longest = elements.get(0);
+        
+        for (String s: elements){
+            if (s.length() > longest.length()){
+                longest = s;
+            }
+        } return longest;
+    }
+
+}
